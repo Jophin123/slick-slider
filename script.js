f9e15f7d5004d55fa0ee0a2633fcc1282c8ff7b7
Jophin123 <jophinfrancis1997@gmail.com>
2021-10-21 15:08:37 +0530

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Basic initialization is like this:
+// $('.your-class').slick();
+
+// I added some other properties to customize my slider
+// Play around with the numbers and stuff to see
+// how it works.
+
+$('.slick-carousel').slick();
